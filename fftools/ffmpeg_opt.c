f1ae886a42ea4b45b7b79f358e7af2adcbcb0d51
youcantgetme <khan.wang@gmail.com>
2020-05-12 07:42:33 +0800

    remove usage notification

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -3259,6 +3259,7 @@ void show_help_default(const char *opt, const char *arg)
 
 void show_usage(void)
 {
+    return;
     av_log(NULL, AV_LOG_INFO, "Hyper fast Audio and Video encoder\n");
     av_log(NULL, AV_LOG_INFO, "usage: %s [options] [[infile options] -i infile]... {[outfile options] outfile}...\n", program_name);
     av_log(NULL, AV_LOG_INFO, "\n");
